94ff884f1c8d602fc46dc6bb9effe21de0ce9bd3
Agência Comunica <agenciacomunicaon@gmail.com>
2026-03-12 15:47:05 -0300

    Add Capacitor configuration and Android platform

---
+++ android/app/src/main/java/com/axol/trrota/MainActivity.java
@@ -0,0 +1,5 @@
+package com.axol.trrota;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.axol.trrota',
+  appName: 'TR Rota Axol',
+  webDir: 'dist'
+};
+
+export default config;
